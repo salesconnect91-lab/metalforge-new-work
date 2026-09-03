3caffb8bf585bc25b629d2644187641e3c3b8a07
salesconnect91-lab <sales.connect91@gmail.com>
2026-09-03 14:16:22 +0300

    Enable ERP-wide darker text contrast

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./auth/AuthContext";
 import "./index.css";
+import "./contrast.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

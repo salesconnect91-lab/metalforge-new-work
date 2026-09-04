4e01951f2a165179ac4e69a10bedf607ee753fc0
salesconnect91-lab <sales.connect91@gmail.com>
2026-09-04 14:37:15 +0300

    Route purchase records to invoice detail workflow

--- src/modules/purchase/Purchase.tsx
+++ src/modules/purchase/Purchase.tsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import PurchaseOrderList from "./PurchaseOrderList";
-import PurchaseOrderDetail from "./PurchaseOrderDetail";
 import MainPurchaseInvoice from "./MainPurchaseInvoice";
 import ConsolidatedPurchaseInvoices from "./ConsolidatedPurchaseInvoices";
+import PurchaseInvoiceDetail from "./PurchaseInvoiceDetail";
 
 export default function Purchase() {
   return (
@@ -10,7 +10,7 @@ export default function Purchase() {
       <Route path="/" element={<PurchaseOrderList />} />
       <Route path="/new" element={<MainPurchaseInvoice />} />
       <Route path="/consolidated" element={<ConsolidatedPurchaseInvoices />} />
-      <Route path="/:id" element={<PurchaseOrderDetail />} />
+      <Route path="/:id" element={<PurchaseInvoiceDetail />} />
     </Routes>
   );
 }

4125076df6bf1291d6cba9080755fa6176a053dc
salesconnect91-lab <sales.connect91@gmail.com>
2026-09-04 16:21:53 +0300

    Apply unified print layout to PDF actions

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./auth/AuthContext";
+import { installUnifiedDocumentOutput } from "./lib/unifiedDocumentOutput";
 import "./index.css";
 import "./contrast.css";
 import "./reportPrint.css";
 import "./invoicePrintFix.css";
+
+installUnifiedDocumentOutput();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

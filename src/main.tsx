6de295cd5929d2b6375eade42fa6dd9d697118e9
salesconnect91-lab <sales.connect91@gmail.com>
2026-09-04 15:02:45 +0300

    Load shared professional report styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import { AuthProvider } from "./auth/AuthContext";
 import "./index.css";
 import "./contrast.css";
+import "./reportPrint.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

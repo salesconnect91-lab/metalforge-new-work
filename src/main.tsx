d294ed9ee84e1f8b08dfde26029340a50c2cd49f
salesconnect91-lab <sales.connect91@gmail.com>
2026-09-04 15:25:46 +0300

    Enable professional invoice print overrides

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { AuthProvider } from "./auth/AuthContext";
 import "./index.css";
 import "./contrast.css";
 import "./reportPrint.css";
+import "./invoicePrintFix.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

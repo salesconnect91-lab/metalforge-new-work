a966671cfd18f1693ca2378be332c1aaf4f6b975
salesconnect91-lab <sales.connect91@gmail.com>
2026-09-04 14:30:01 +0300

    Route main and consolidated purchase invoices separately

--- src/modules/purchase/Purchase.tsx
+++ src/modules/purchase/Purchase.tsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import PurchaseOrderList from "./PurchaseOrderList";
 import PurchaseOrderDetail from "./PurchaseOrderDetail";
-import CreatePurchaseInvoice from "./CreatePurchaseInvoice";
+import MainPurchaseInvoice from "./MainPurchaseInvoice";
+import ConsolidatedPurchaseInvoices from "./ConsolidatedPurchaseInvoices";
 
 export default function Purchase() {
   return (
     <Routes>
       <Route path="/" element={<PurchaseOrderList />} />
-      <Route path="/new" element={<CreatePurchaseInvoice />} />
+      <Route path="/new" element={<MainPurchaseInvoice />} />
+      <Route path="/consolidated" element={<ConsolidatedPurchaseInvoices />} />
       <Route path="/:id" element={<PurchaseOrderDetail />} />
     </Routes>
   );

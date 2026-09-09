37e7329d48f87bd03ec97647b58367081d827a9b
salesconnect91-lab <sales.connect91@gmail.com>
2026-09-09 08:38:55 +0300

    Remove superseded sales runtime bridges

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,13 @@ import BusinessUnitSwitcher from "@/components/BusinessUnitSwitcher";
 import ErpExperienceBridge from "@/components/ErpExperienceBridge";
 import GlobalFormStandards from "@/components/GlobalFormStandards";
 import PrintPreviewController from "@/components/PrintPreviewController";
-import InvoiceCommercialControls from "@/components/InvoiceCommercialControls";
 import DashboardGlobalSearch from "@/components/DashboardGlobalSearch";
 import OrderBookQuickAccess from "@/components/OrderBookQuickAccess";
 import OrderBookExportToolbar from "@/components/OrderBookExportToolbar";
 import SalesOrderBookDraftBridge from "@/components/SalesOrderBookDraftBridge";
 import UnifiedOrderBookInvoicePicker from "@/components/UnifiedOrderBookInvoicePicker";
-import SalesInvoiceEmployeeSalespersonBridge from "@/components/SalesInvoiceEmployeeSalespersonBridge";
-import ReliableInvoiceRowDelete from "@/components/ReliableInvoiceRowDelete";
-import DraftSalesOperationalBridge from "@/components/DraftSalesOperationalBridge";
 import OrderBookRateRevisionBridge from "@/components/OrderBookRateRevisionBridge";
 import OrderBookCustomerReport from "@/components/OrderBookCustomerReport";
-import SalesInvoicePrintStatusBridge from "@/components/SalesInvoicePrintStatusBridge";
 import OrderBookCancelControl from "@/components/OrderBookCancelControl";
 import OrderBookQtyAdjustControl from "@/components/OrderBookQtyAdjustControl";
 import OrderBookActionHub from "@/components/OrderBookActionHub";
@@ -85,17 +80,12 @@ function GlobalExperience() {
     <ErpExperienceBridge />
     <GlobalFormStandards />
     <PrintPreviewController />
-    <InvoiceCommercialControls />
     <OrderBookQuickAccess />
     <OrderBookExportToolbar />
     <SalesOrderBookDraftBridge />
     <UnifiedOrderBookInvoicePicker />
-    <SalesInvoiceEmployeeSalespersonBridge />
-    <ReliableInvoiceRowDelete />
-    <DraftSalesOperationalBridge />
     <OrderBookRateRevisionBridge />
     <OrderBookCustomerReport />
-    <SalesInvoicePrintStatusBridge />
     <OrderBookCancelControl />
     <OrderBookQtyAdjustControl />
     <OrderBookActionHub />

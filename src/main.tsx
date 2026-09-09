b3e1e0705992981774c7691bdce145f57e9a081f
salesconnect91-lab <sales.connect91@gmail.com>
2026-09-09 23:07:35 +0300

    Load Order Book modal sizing fix

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import "./contrast.css";
 import "./reportPrint.css";
 import "./naviloDocumentPrint.css";
 import "./naviloEnterprisePrint.css";
+import "./orderBook.css";
 import "./accountingStatements.css";
 
 createRoot(document.getElementById("root")!).render(

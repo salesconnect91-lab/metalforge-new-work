e9a53d45f93f3a1a614a402046c9850a5ec61e92
salesconnect91-lab <sales.connect91@gmail.com>
2026-09-12 16:21:47 +0300

    Add loan ledger shortcut to Cash Counter

--- src/modules/accounting/CashCounterWorkspace.tsx
+++ src/modules/accounting/CashCounterWorkspace.tsx
@@ -6,6 +6,7 @@ export default function CashCounterWorkspace(){
     <div className="no-print flex flex-wrap gap-2 rounded-xl border bg-white p-3">
       <NavLink to="/accounting/cash-counter" className={({isActive})=>isActive?"btn-primary":"btn-secondary"}>Cash Counter</NavLink>
       <NavLink to="/accounting/payroll" className="btn-secondary">Payroll & Salary Ledger</NavLink>
+      <NavLink to="/accounting/loans" className="btn-secondary">Loan & Lender Ledger</NavLink>
     </div>
     <CashCounter />
   </div>;

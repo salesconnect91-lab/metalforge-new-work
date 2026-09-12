734e025047ac20cd27305c6fb2b321d1d5fea9d1
salesconnect91-lab <sales.connect91@gmail.com>
2026-09-12 16:58:05 +0300

    Load payroll and loan ledger navigation

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { AuthProvider } from "./auth/AuthContext";
 import LanguageRuntime from "./components/LanguageRuntime";
 import "./printTargetRuntime";
 import "./accountNameDisplayRuntime";
+import "./accountingLedgerNavigationRuntime";
 import "./languageIsolationRuntime";
 import "./documentLanguageIsolationRuntime";
 import "./index.css";

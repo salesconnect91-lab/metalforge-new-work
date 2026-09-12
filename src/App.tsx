105d2352a3abcb53f8806e552f14441a09c561f5
salesconnect91-lab <sales.connect91@gmail.com>
2026-09-12 15:58:16 +0300

    Link salesperson performance to salesperson ledger

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import MasterData from "@/modules/master-data/MasterData";
 import SalesInvoiceList from "@/modules/sales/SalesInvoiceList";
 import SalesInvoiceCreate from "@/modules/sales/SalesInvoiceCreate";
 import SalesInvoiceDetail from "@/modules/sales/SalesInvoiceDetail";
-import SalespersonReport from "@/modules/sales/SalespersonReport";
+import SalespersonReportHub from "@/modules/sales/SalespersonReportHub";
 import SalespersonLedger from "@/modules/sales/SalespersonLedger";
 import ChargeMaster from "@/modules/sales/ChargeMaster";
 import Purchase from "@/modules/purchase/Purchase";
@@ -105,7 +105,7 @@ export default function App() {
       <Route path="/sales" element={<ModuleOnly module="sales"><SalesInvoiceList /></ModuleOnly>} />
       <Route path="/sales/new" element={<ModuleActionOnly module="sales" action="create"><SalesInvoiceCreate /></ModuleActionOnly>} />
       <Route path="/sales/:id/edit" element={<ModuleActionOnly module="sales" action="edit"><SalesInvoiceCreate /></ModuleActionOnly>} />
-      <Route path="/sales/report" element={<ModuleOnly module="reports"><ReportSurface><SalespersonReport /></ReportSurface></ModuleOnly>} />
+      <Route path="/sales/report" element={<ModuleOnly module="reports"><ReportSurface><SalespersonReportHub /></ReportSurface></ModuleOnly>} />
       <Route path="/sales/person-ledger" element={<ModuleOnly module="reports"><ReportSurface><SalespersonLedger /></ReportSurface></ModuleOnly>} />
       <Route path="/sales/charges" element={<ModuleOnly module="master"><ChargeMaster /></ModuleOnly>} />
       <Route path="/sales/consolidated" element={<ModuleOnly module="sales"><ConsolidatedInvoices /></ModuleOnly>} />
